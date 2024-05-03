a85c9073702aeab6ed22481d6cd29ed6c17601d6
gozneokhan <gozneokhan@gmail.com>
2024-05-04 02:10:01 +0900

    feat: todolist-ui-implement

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,17 @@
+import './List.css';
+import TodoItem from './TodoItem';
+
 const List = () => {
-    return <div>List</div>;
+    return (
+        <div className="List">
+            <h4>Todo List🌿</h4>
+            <input placeholder="검색어를 입력하세요" />
+            <div className="todos_wrapper">
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+            </div>
+        </div>
+    );
 };
 export default List;
